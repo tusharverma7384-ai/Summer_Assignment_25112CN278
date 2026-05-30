19b74da5cf2b4a6acea5cb1261a6db59804c0b86
tusharverma7384-ai <tusharverma7384@gmail.com>
2026-05-30 21:50:24 +0530

    Create factorial.cpp

---
+++ Day 1/factorial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int n, fact = 1;
+     cout<<"Enter a number: ";
+     cin>>n;
+     for(int i = 2; i<=n; i++){
+        fact *=i;
+     }
+     cout<<fact;
+     return 0;
+}
