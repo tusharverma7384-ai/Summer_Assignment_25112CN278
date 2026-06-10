9cbc687eca8cecf4851d380fbb4ea2e0ce7c7608
tusharverma7384-ai <tusharverma7384@gmail.com>
2026-06-10 22:14:01 +0530

    Create starPyramid.cpp

---
+++ Day 10/starPyramid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+   for(int i = 1; i<=5; i++){
+      for(int k = i; k<=4; k++){
+         cout<<" ";
+      }
+      for(int j = 1; j<=(2*i-1); j++){
+         cout<<"*";
+      }
+      cout<<endl;
+   }
+}
