6da8adf29745b0b74ad288f36c25fe14a49c6a8a
tusharverma7384-ai <tusharverma7384@gmail.com>
2026-06-25 23:56:39 +0530

    Create sortbyLength.cpp

---
+++ Day 25/sortbyLength.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of names: ";
+    cin>>n;
+    string names[n];
+    cout<<"Enter names: ";
+    for(int i = 0; i<n; i++){
+        getline(cin>>ws, names[i]);
+    }
+    bool swapped;
+    for(int i = 0; i<n-1; i++){
+        swapped = false;
+        for(int j = 0; j<n-i-1; j++){
+            if(names[j].length()>names[j+1].length()){
+                string t = names[j+1];
+                names[j+1] = names[j];
+                names[j] = t;
+                swapped = true;
+            }
+        }
+        if(!swapped)
+            break;
+    }
+    for(int i = 0; i<n; i++){
+        cout<<names[i]<<" ";
+    }
+}
